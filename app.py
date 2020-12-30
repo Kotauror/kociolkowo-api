ca1b92f94c1897e718ef7f3ce062caa74818fa8e
Kotauror <zygmunt.justyna@gmail.com>
2020-12-30 11:37:21 +0000

    Start adding get all churches.

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask
+# app.py
+
+import os
+
+import boto3
+
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+CHURCHES_TABLE = os.environ['CHURCHES_TABLE']
+client = boto3.client('dynamodb')
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/churches")
+def get_all_churches():
+    resp = client.get_item(
+        TableName=CHURCHES_TABLE,
+        Key={
+            'churchId': { 'S': churchId }
+        }
+    )
+    item = resp.get('Item')
+    if not item:
+        return jsonify({'error': 'No churches found'}), 404
+
+    return jsonify({
+        'churchId': item.get('churchId').get('S'),
+        'name': item.get('name').get('S')
+    })
+
+  #  https://www.serverless.com/blog/flask-python-rest-api-serverless-lambda-dynamodb
+  # get all churches
