00fd54a6a558d3c10eb42fc6f8b9acf3ad295b27
Kotauror <zygmunt.justyna@gmail.com>
2020-12-30 16:56:27 +0000

    Add adding church.

--- app.py
+++ app.py
@@ -10,26 +10,38 @@ app = Flask(__name__)
 CHURCHES_TABLE = os.environ['CHURCHES_TABLE']
 client = boto3.client('dynamodb')
 
+# dynamodb = boto3.resource('dynamodb', aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_ACCESS_KEY,region_name="eu-west-2")
+# churches_table = dynamodb.Table(CHURCHES_TABLE)
+
+# AWS_ACCESS_KEY = os.getenv('KOS_AK')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('KOS_SAK')
+
+# c = boto3.dynamodb2.connect_to_region(aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_ACCESS_KEY,region_name="eu-west-2")
+# churches_table = Table(CHURCHES_TABLE,connection=c)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/churches")
-def get_all_churches():
-    resp = client.get_item(
+@app.route("/churches", methods=["POST"])
+def create_church():
+    church_id = request.json.get('churchId')
+    church_name = request.json.get('name')
+    if not church_id or not church_name:
+        return jsonify({'error': 'Please provide userId and name'}), 400
+
+    resp = client.put_item(
         TableName=CHURCHES_TABLE,
-        Key={
-            'churchId': { 'S': churchId }
+        Item={
+            'churchId': {'S': church_id },
+            'name': {'S': church_name }
         }
     )
-    item = resp.get('Item')
-    if not item:
-        return jsonify({'error': 'No churches found'}), 404
 
     return jsonify({
-        'churchId': item.get('churchId').get('S'),
-        'name': item.get('name').get('S')
+        'churchId': church_id,
+        'name': church_name
     })
 
   #  https://www.serverless.com/blog/flask-python-rest-api-serverless-lambda-dynamodb
